a8fe911ddc111b3fd6458d2aed2503c66880b277
Sergey Mazovets <smazovets@gmail.com>
2022-01-26 23:36:01 +0700

    java3.lesson2 complete

--- src/main/java/ru/geekbrains/java/service/implementation/BaseAuthService.java
+++ src/main/java/ru/geekbrains/java/service/implementation/BaseAuthService.java
@@ -1,52 +1,69 @@
 package ru.geekbrains.java.service.implementation;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+
 import ru.geekbrains.java.service.AuthService;
+import ru.geekbrains.java.service.DBConnection;
 
 public class BaseAuthService implements AuthService {
 
-  private static class Entry {
+    private static class Entry {
 
-    private final String login;
-    private final String pass;
-    private final String nick;
+        private final String login;
+        private final String pass;
+        private final String nick;
 
-    public Entry(String login, String pass, String nick) {
-      this.login = login;
-      this.pass = pass;
-      this.nick = nick;
+        public Entry(String login, String pass, String nick) {
+            this.login = login;
+            this.pass = pass;
+            this.nick = nick;
+        }
     }
-  }
 
-  private final List<Entry> entries;
+    private final List<Entry> entries;
 
-  @Override
-  public void start() {
-    System.out.println("Сервис аутентификации запущен");
-  }
-
-  @Override
-  public void stop() {
-    System.out.println("Сервис аутентификации остановлен");
-  }
-
-  @Override
-  public String loginUser(String login, String pass) {
-    for (Entry o : entries) {
-      if (o.login.equals(login) && o.pass.equals(pass)) {
-        return o.nick;
-      }
+    @Override
+    public void start() {
+        System.out.println("Сервис аутентификации запущен");
     }
-    return null;
-  }
 
-  public BaseAuthService() {
-    entries = new ArrayList<>();
-    entries.add(new Entry("login1", "pass1", "nick1"));
-    entries.add(new Entry("login2", "pass2", "nick2"));
-    entries.add(new Entry("login3", "pass3", "nick3"));
-  }
+    @Override
+    public void stop() {
+        System.out.println("Сервис аутентификации остановлен");
+    }
+
+    @Override
+    public String loginUser(String login, String pass) {
+        for (Entry o : entries) {
+            if (o.login.equals(login) && o.pass.equals(pass)) {
+                return o.nick;
+            }
+        }
+        return null;
+    }
+
+    public BaseAuthService() {
+        entries = new ArrayList<>();
+        Connection dbCon;
+
+        try {
+            dbCon = DBConnection.getInstance();
+            Statement stmt = dbCon.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM users;");
+            while (rs.next()) {
+                entries.add(new Entry(rs.getString("login"), rs.getString("pass"), rs.getString("nick")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        DBConnection.disconnect();
+    }
 
 }
 
